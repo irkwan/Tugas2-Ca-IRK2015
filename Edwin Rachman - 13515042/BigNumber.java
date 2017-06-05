e46a9731e241b8bafaf3fa7dc6ecbbbd0c619e23
a-iueo <edwin.rachman@ymail.com>
2017-06-05 22:49:34 +0700

    BigNumber class - changed constructor implementations - removed isNegative method - added getSize method - changed parts.length attribute calls to getSize method - added setLongPart method - added getPartAsLong method - refactored add method implementation using setLongPart and getPartAsLong methods - added BigNumber * int multiply method - added naive multiply method (todo: change implementation to Karatsuba) - changed add, inverse, negate, substract methods to static functions - added add, inverse, negate, substract methods using their respective static functions

--- Edwin Rachman - 13515042/BigNumber.java
+++ Edwin Rachman - 13515042/BigNumber.java
@@ -3,65 +3,148 @@ import java.util.ArrayList;
 public class BigNumber {
   private int[] parts;
 
-  public BigNumber (int size) {
+  public BigNumber (int size, int value, int shift) {
     parts = new int[size];
     for (int i = 0; i < size; ++i) {
-      parts[i] = 0;
+      parts[i] = i == shift ? value : 0;
     }
   }
 
   public BigNumber (int size, int value) {
-    parts = new int[size];
-    parts[0] = value;
-    for (int i = 1; i < size; ++i) {
-      parts[i] = 0;
-    }
+    this(size, value, 0);
   }
 
-  public BigNumber (int size, BigNumber obj) {
-    parts = new int[size];
-    for (int i = 0; i < size; ++i) {
-      parts[i] = i < obj.parts.length ? obj.parts[i] : 0;
-    }
+  public BigNumber (int size) {
+    this(size, 0, 0);
   }
 
-  public boolean isNegative () {
-    return parts[parts.length - 1] < 0;
+  public BigNumber (BigNumber obj) {
+    parts = new int[obj.parts.length];
+    System.arraycopy(obj.parts, 0, parts, 0, obj.parts.length);
+  }
+
+  public int getSize () {
+    return parts.length;
   }
 
   public String toHexString () {
     StringBuilder builder = new StringBuilder();
-    for (int i = parts.length - 1; i >= 0; --i) {
+    for (int i = getSize() - 1; i >= 0; --i) {
       builder.append(String.format("%08X ", parts[i]));
     }
     return builder.toString();
   }
 
-  public BigNumber add (int size, BigNumber other) {
-    BigNumber result = new BigNumber(size);
-    int carry = 0;
-    for (int i = 0; i < size; ++i) {
-      long sum = (i < parts.length ? Integer.toUnsignedLong(parts[i]) : 0) + (i < other.parts.length ? Integer.toUnsignedLong(other.parts[i]) : 0) + carry;
-      result.parts[i] = (int) sum;
-      carry = sum > 0xFFFFFFFFL ? 1 : 0;
+  public void setLongPart (long value, int shift) {
+    parts[shift] = (int) value;
+    if (shift + 1 < getSize()) {
+      parts[shift + 1] = (int) (value >>> 32);
+    }
+  }
+
+  public long getPartAsLong (int shift) {
+    return shift < getSize() ? Integer.toUnsignedLong(parts[shift]) : 0;
+  }
+
+  public static BigNumber add (BigNumber x, BigNumber y) {
+    BigNumber result = new BigNumber(Math.max(x.getSize(), y.getSize()));
+    for (int i = 0; i < result.getSize(); ++i) {
+      result.setLongPart(x.getPartAsLong(i) + y.getPartAsLong(i) + result.getPartAsLong(i), i);
     }
     return result;
   }
 
-  public BigNumber inverse (int size) {
-    BigNumber result = new BigNumber(size, this);
-    for (int i = 0; i < size; ++i) {
+  public BigNumber add (BigNumber other) {
+    return add(this, other);
+  }
+
+  public static BigNumber multiply (BigNumber x, long y) {
+    BigNumber result = new BigNumber(x.getSize());
+    for (int i = 0; i < result.getSize(); ++i) {
+      result.setLongPart(x.getPartAsLong(i) * y + result.getPartAsLong(i), i);
+    }
+    return result;
+  }
+
+  public BigNumber multiply (long other) {
+    return multiply(this, other);
+  }
+
+  public static BigNumber multiply (BigNumber x, BigNumber y) {
+    BigNumber result = new BigNumber(Math.max(x.getSize(), y.getSize()));
+    for (int i = 0; i < result.getSize(); ++i) {
+      result = result.add(x.multiply(y.getPartAsLong(i)).shift(i));
+    }
+    return result;
+  }
+
+  public BigNumber multiply (BigNumber other) {
+    return multiply(this, other);
+  }
+
+  public static BigNumber inverse (BigNumber x) {
+    BigNumber result = new BigNumber(x);
+    for (int i = 0; i < result.getSize(); ++i) {
       result.parts[i] = ~result.parts[i];
     }
     return result;
   }
 
-  public BigNumber negate (int size) {
-    return inverse(size).add(size, new BigNumber(size, 1));
+  public BigNumber inverse () {
+    return inverse(this);
   }
 
+  public static BigNumber negate (BigNumber x) {
+    return inverse(x).add(new BigNumber(1, 1));
+  }
+
+  public BigNumber negate () {
+    return negate(this);
+  }
+
+  public static BigNumber substract (BigNumber x, BigNumber y) {
+    return x.add(y.negate());
+  }
+
+  public BigNumber substract (BigNumber other) {
+    return substract(this, other);
+  }
+
+  public static BigNumber shift (BigNumber x, int shift) {
+    BigNumber result = new BigNumber(x);
+    for (int i = 0; i < shift; ++i) {
+      result.parts[i] = 0;
+    }
+    System.arraycopy(x.parts, 0, result.parts, shift, result.getSize() - shift);
+    return result;
+  }
+
+  public BigNumber shift (int shift) {
+    return shift(this, shift);
+  }
+
+  /*public static BigNumber multiply (BigNumber x, BigNumber y) {
+    if (x.getSize() == 1 || y.getSize() == 1) {
+      return new BigNumber(2, x.getPartAsLong(0) * y.getPartAsLong(0));
+    }
+    else {
+
+
+      BigNumber z0 = multiply(x0, y0);
+      BigNumber z2 = multiply(x1, y1);
+      BigNumber z1 = multiply(x0.add(x1), y0.add(y1)).substract(z0.add(z1));
+
+      return z2.shift(m * 2).add(z1.shift(m)).add(z0);
+    }
+  }*/
+
   public static void main (String[] args) {
-    BigNumber x = new BigNumber(4, 0xFFFFFFFF);
-    System.out.println(x.add(4, x).toHexString());
+    BigNumber x = new BigNumber(16, -1);
+    x = x.multiply(x);
+    x = x.multiply(x);
+    x = x.multiply(x);
+    x = x.multiply(x);
+    x = x.substract(new BigNumber(16, -2));
+    System.out.println(x.toHexString());
   }
 }
