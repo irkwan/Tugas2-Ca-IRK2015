6b8621945251886c53ec73bf5978c4f39760ce95
wijayaerick <wijaya.erick52@gmail.com>
2017-06-08 22:45:00 +0700

    will code in cpp rather thhan java

---
+++ Erick Wijaya - 13515057/biginteger.cpp
@@ -0,0 +1,95 @@
+// NIM/Nama : 13515057 / Erick Wijaya
+// File     : biginteger.cpp
+
+#include "biginteger.h"
+using namespace std;
+
+/* Constructors */
+biginteger::biginteger(){
+	sign = true;
+}
+
+biginteger::biginteger(long long v){
+	sign = v >= 0;
+	while (v != 0){
+		
+	}
+}
+
+biginteger::biginteger(const string &v){
+
+}
+
+biginteger::biginteger(const biginteger &v){
+
+}
+
+
+/* Destructor */
+biginteger::~biginteger(){
+
+}
+
+
+/* Operator= */
+biginteger& biginteger::operator=(const biginteger &rhs){
+
+}
+
+
+/* Arithmetic Operators */
+biginteger biginteger::add(const biginteger &rhs){
+
+}
+
+biginteger biginteger::min(const biginteger &rhs){
+
+}
+
+biginteger biginteger::mul(const biginteger &rhs){
+
+}
+
+biginteger biginteger::div(const biginteger &rhs){
+
+}
+
+biginteger biginteger::mod(const biginteger &rhs){
+
+}
+
+
+biginteger biginteger::operator+(const biginteger &rhs){
+
+}
+
+biginteger biginteger::operator-(const biginteger &rhs){
+
+}
+
+biginteger biginteger::operator*(const biginteger &rhs){
+
+}
+
+biginteger biginteger::operator/(const biginteger &rhs){
+
+}
+
+biginteger biginteger::operator%(const biginteger &rhs){
+
+}
+
+
+/* Relational Operators */
+bool biginteger::operator==(const biginteger &rhs){
+
+}
+
+bool biginteger::operator>(const biginteger &rhs){
+
+}
+
+bool biginteger::operator<(const biginteger &rhs){
+
+}
+
