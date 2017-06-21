95d3b2efd4be354183117258eda62eae123e3a6c
dewitast <dewitast20@gmail.com>
2017-06-21 19:05:51 +0700

    optimize basic arithmetic

--- Dewita Sonya T. - 13515021/bignumber.cpp
+++ Dewita Sonya T. - 13515021/bignumber.cpp
@@ -216,14 +216,7 @@ BigNumber& BigNumber::operator*=(BigNumber number) {
 	bool neg = negative != number.negative;
 	negative = false;
 	number.negative = false;
-	int size1 = size(), size2 = number.size(), sze;
-	if (size1 > size2) {
-		number >> (size1 - size2);
-		sze = size1;
-	} else {
-		operator>>(size2 - size1);
-		sze = size2;
-	}
+	int sze = max(size(), number.size());
 	if (sze == 1) {
 		int result = num[0] * number.num[0];
 		num[0] = result;
@@ -233,12 +226,11 @@ BigNumber& BigNumber::operator*=(BigNumber number) {
 		}
 	} else { //Karatsuba
 		int m = (sze + 1) / 2;
-		BigNumber x = Truncate(m), y = number.Truncate(m), z = *this + x, z0 = x * y;
+ 		BigNumber x = Truncate(m), y = number.Truncate(m), z = *this + x, z2 = x * y;
 		operator*=(number);
-		BigNumber z1 = (z * (y + number)) - z0 - *this;
-		operator<<(2*m);
+		BigNumber z1 = (z * (y + number)) - z2 - *this;
 		operator+=(z1 << m);
-		operator+=(z0);
+		operator+=(z2 << 2*m);
 	}
 	negative = neg;
 	if (num[0] == 0) {
@@ -248,17 +240,16 @@ BigNumber& BigNumber::operator*=(BigNumber number) {
 }
 
 BigNumber& BigNumber::operator%=(const BigNumber& number) {
-	BigNumber temp(*this), dig;
+	BigNumber temp(*this);
 	num.clear();
 	num.push_back(0);
 	negative = false;
 	for (int i = 0; i < temp.size(); ++i) {
-		operator<<(1);
-		dig.num[0] = temp.num[i];
-		operator+=(dig);
-		while (operator>=(number)) {
+		num.push_back(temp.num[i]);
+		while (!negative) {
 			operator-=(number);
 		}
+		operator+=(number);
 	}
 	return *this;
 }
@@ -271,11 +262,15 @@ BigNumber& BigNumber::ModPow(BigNumber pow, const BigNumber& mod) {
 	} else {
 		operator%=(mod);
 		BigNumber base(*this);
-		pow--;
+		--pow;
 		while (pow.num[0] != 0) {
-			operator*=(base);
-			operator%=(mod);
-			pow--;
+			if (pow.num[pow.num.size() - 1] % 2 == 1) {
+				operator*=(base);
+				operator%=(mod);
+			}
+			pow >> 1;
+			base.operator*=(base);
+			base.operator%=(mod);
 		}
 	}
 	return *this;
@@ -386,20 +381,22 @@ BigNumber& BigNumber::operator<<(int len) {
 			for (int i = 0; i < len; ++i) {
 				num.push_back(0);
 			}
-		} else {
-			operator>>(-len);
 		}
 	}
 	return *this;
 }
 
 BigNumber& BigNumber::operator>>(int len) {
-	if (len >= 0) {
-		for (int i = 0; i < len; ++i) {
-			num.insert(num.begin(),0);
+	for (int i = 0; i < len; ++i) {
+		int carry = 0, sze = size();
+		for (int i = 0; i < sze; ++i) {
+			num[i] += carry;
+			carry = 10 * (num[i] % 2 == 1);
+			num[i] /= 2;
 		}
-	} else {
-		operator<<(-len);
+		if (num[0] == 0 && size() > 1) {
+			num.erase(num.begin());
+		}
 	}
 	return *this;
 }
@@ -407,38 +404,41 @@ BigNumber& BigNumber::operator>>(int len) {
 /* Random */
 BigNumber BigNumber::Random() const {
 	BigNumber temp;
-	srand((unsigned)time(0));
-	temp.num[0] = rand() % num[0];
-	for (int i = 1; i < size(); ++i) {
-		temp.num.push_back(rand() % 10);
+	int sze = size();
+	if (sze == 1) {
+		temp.num[0] == num[0] - 2;
+		if (temp.num[0] < 2) {
+			temp.num[0] += 4;
+		}
+	} else {
+		int digit = rand() % (sze - 1) + 1;
+		temp.num[0] = rand() % 9 + 1;
+		for (int i = 1; i < digit; ++i) {
+			temp.num.push_back(rand() % 10);
+		}
+		if (digit == 1 && temp.num[0] == 1) {
+			temp.num[0]++;
+		}
 	}
-	while (!temp.num.empty() && temp.num[0] == 0) {
-		temp.num.erase(temp.num.begin());
-	}
-	if (temp.num.empty() || (temp.num.size() == 1 && temp.num[0] == 1)) {
-		temp.num.resize(1);
-		temp.num[0] = 2;
-	}
-	temp.negative = negative;
 	return temp;
 }
 
 BigNumber BigNumber::GenerateRandomPrime(int digit) { //Fermat Primality Testing
-	BigNumber temp, cek, one("1");
+	BigNumber temp, cek, one("1"), two("2");
 	bool prime = false, test;
 	srand((unsigned)time(0) + digit);
+	temp.num[0] = rand() % 9 + 1;
+	for (int i = 1; i < digit - 1; ++i) {
+		temp.num.push_back(rand() % 10);
+	}
+	temp.num.push_back(2 * (rand() % 5) + 1);
+	BigNumber temp2 = temp - one;
 	while (!prime) {
-		temp.num.clear();
-		temp.num.push_back(rand() % 9 + 1);
-		for (int i = 1; i < digit - 1; ++i) {
-			temp.num.push_back(rand() % 10);
-		}
-		temp.num.push_back(2 * (rand() % 5) + 1);
 		int i = 0;
 		test = true;
 		while (test && i < 5) {
 			cek = temp.Random();
-			cek.ModPow(temp - one, temp);
+			cek.ModPow(temp2, temp);
 			if (cek.size() != 1 || cek.num[0] != 1) {
 				test = false;
 			}
@@ -446,6 +446,9 @@ BigNumber BigNumber::GenerateRandomPrime(int digit) { //Fermat Primality Testing
 		}
 		if (test) {
 			prime = true;
+		} else {
+			temp -= two;
+			temp2 -= two;
 		}
 	}
 	return temp;
@@ -463,27 +466,14 @@ BigNumber BigNumber::Negate() const {
 
 BigNumber BigNumber::Truncate(int len) {
 	BigNumber temp;
-	if (len > 0 && len < size()) {
+	int sze = size();
+	if (len < sze) {
+		sze -= len;
 		temp.num.pop_back();
-		for (int i = 0; i < len; ++i) {
-			temp.num.insert(temp.num.begin(), num[num.size()-1]);
-			num.pop_back();
+		for (int i = 0; i < sze; ++i) {
+			temp.num.push_back(num[0]);
+			num.erase(num.begin());
 		}
 	}
-	while (!num.empty() && num[0] == 0) {
-		num.erase(num.begin());
-	}
-	if (num.empty()) {
-		negative = false;
-		num.push_back(0);
-	}
-	while (!temp.num.empty() && temp.num[0] == 0) {
-		temp.num.erase(temp.num.begin());
-	}
-	if (temp.num.empty()) {
-		temp.negative = false;
-		temp.num.push_back(0);
-	}
-	temp.negative = negative;
 	return temp;
 }
