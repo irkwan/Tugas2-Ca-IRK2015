8c3358ffdbfca20c4e3211aa151af7e40766ba6e
a-iueo <edwin.rachman@ymail.com>
2017-06-22 23:28:33 +0700

    Add modularExponent, Remove static methods

--- Edwin Rachman - 13515042/BigNumber.java
+++ Edwin Rachman - 13515042/BigNumber.java
@@ -33,19 +33,62 @@ public class BigNumber {
     System.arraycopy(obj.parts, 0, parts, 0, obj.parts.length);
   }
 
-  public BigNumber arithmeticExtend (int size) {
-    if (size > getSize()) {
-      BigNumber result = new BigNumber(size);
-      System.arraycopy(parts, 0, result.parts, 0, parts.length);
-      boolean isNeg = parts[getSize() - 1] < 0;
-      for (int i = getSize(); i < result.getSize(); ++i) {
-        result.parts[i] = isNeg ? -1 : 0;
+  public boolean moreThan(BigNumber y) {
+    return compare(y) > 0;
+  }
+
+  public boolean moreOrEqualTo(BigNumber y) {
+    return compare(y) >= 0;
+  }
+
+  public boolean equalTo(BigNumber y) {
+    return compare(y) <= 0;
+  }
+
+  public boolean lessOrEqualTo(BigNumber y) {
+    return compare(y) <= 0;
+  }
+
+  public boolean lessThan(BigNumber y) {
+    return compare(y) < 0;
+  }
+
+  public BigNumber inverse() {
+    BigNumber result = new BigNumber(this);
+    for (int i = 0; i < result.getSize(); ++i) {
+      result.parts[i] = ~result.parts[i];
+    }
+    return result;
+  }
+
+  public BigNumber leftPartShift(int shift) {
+    BigNumber result = new BigNumber(getSize());
+    for (int i = 0; i < shift; ++i) {
+      result.parts[i] = 0;
+    }
+    System.arraycopy(parts, 0, result.parts, shift, getSize() - shift);
+    return result;
+  }
+
+  public BigNumber rightPartShift(int shift) {
+    BigNumber result = new BigNumber(getSize());
+    for (int i = shift; i < getSize(); ++i) {
+      result.parts[i] = 0;
+    }
+    System.arraycopy(parts, shift, result.parts, 0, getSize() - shift);
+    return result;
+  }
+
+  public int compare(BigNumber y) {
+    for (int i = getSize() - 1; i >= 0; --i) {
+      if (getPartAsLong(i) < y.getPartAsLong(i)) {
+        return -1;
       }
-      return result;
+      else if (getPartAsLong(i) > y.getPartAsLong(i)) {
+        return 1;
+      }
     }
-    else {
-      return this;
-    }
+    return 0;
   }
 
   public int getSize() {
@@ -89,34 +132,30 @@ public class BigNumber {
     return result;
   }
 
-  public static BigNumber partShift(BigNumber x, int shift) {
-    BigNumber result = new BigNumber(x.getSize());
-    for (int i = 0; i < shift; ++i) {
-      result.parts[i] = 0;
-    }
-    System.arraycopy(x.parts, 0, result.parts, shift, x.getSize() - shift);
-    return result;
-  }
-
-  public static BigNumber bitShift(BigNumber x, int shift) {
+  public BigNumber leftBitShift (int shift) {
     if (shift < 32) {
-      BigNumber result = new BigNumber(x.getSize());
+      BigNumber result = new BigNumber(getSize());
       for (int i = 0; i < result.getSize(); ++i) {
-        result.setLongPart(x.getPartAsLong(i) << shift | result.getPartAsLong(i), i);
+        result.setLongPart(getPartAsLong(i) << shift | result.getPartAsLong(i), i);
       }
       return result;
     }
     else {
-      return x.partShift(shift / 32).bitShift(shift % 32);
+      return leftPartShift(shift / 32).leftBitShift(shift % 32);
     }
   }
 
-  public static BigNumber inverse(BigNumber x) {
-    BigNumber result = new BigNumber(x);
-    for (int i = 0; i < result.getSize(); ++i) {
-      result.parts[i] = ~result.parts[i];
+  public BigNumber rightBitShift (int shift) {
+    if (shift < 32) {
+      BigNumber result = new BigNumber(getSize());
+      for (int i = result.getSize() - 1; i >= 0 ; --i) {
+        result.setLongPart(getPartAsLong(i) >> shift | result.getPartAsLong(i), i);
+      }
+      return result;
     }
-    return result;
+    else {
+      return rightPartShift(shift / 32).rightBitShift(shift % 32);
+    }
   }
 
   public boolean getBit (int shift) {
@@ -132,64 +171,55 @@ public class BigNumber {
   }
 
 
-  public static BigNumber add (BigNumber x, long y) {
-    BigNumber result = new BigNumber(x.getSize());
-    result.setLongPart(x.getPartAsLong(0) + y, 0);
+  public BigNumber add (long y) {
+    BigNumber result = new BigNumber(getSize());
+    result.setLongPart(getPartAsLong(0) + y, 0);
     for (int i = 1; i < result.getSize(); ++i) {
-      result.setLongPart(x.getPartAsLong(i) + result.getPartAsLong(i), i);
+      result.setLongPart(getPartAsLong(i) + result.getPartAsLong(i), i);
     }
     return result;
   }
 
-  public static BigNumber add(BigNumber x, BigNumber y) {
-    BigNumber result = new BigNumber(Math.max(x.getSize(), y.getSize()));
+  public BigNumber add (BigNumber y) {
+    BigNumber result = new BigNumber(getSize());
     for (int i = 0; i < result.getSize(); ++i) {
-      result.setLongPart(x.getPartAsLong(i) + y.getPartAsLong(i) + result.getPartAsLong(i), i);
+      result.setLongPart(getPartAsLong(i) + y.getPartAsLong(i) + result.getPartAsLong(i), i);
     }
     return result;
   }
 
-  public static BigNumber negate(BigNumber x) {
-    return inverse(x).add(1);
+  public BigNumber negate () {
+    return inverse().add(1);
   }
 
-  public static BigNumber subtract(BigNumber x, BigNumber y) {
-    return x.add(y.negate());
+  public BigNumber subtract (BigNumber y) {
+    return add(y.negate());
   }
 
-  public static BigNumber multiply(BigNumber x, long y) {
-    BigNumber result = new BigNumber(x.getSize());
+  public BigNumber multiply (long y) {
+    BigNumber result = new BigNumber(getSize());
     for (int i = 0; i < result.getSize(); ++i) {
-      result.setLongPart(x.getPartAsLong(i) * y + result.getPartAsLong(i), i);
+      result.setLongPart(getPartAsLong(i) * y + result.getPartAsLong(i), i);
     }
     return result;
   }
 
-  public static BigNumber multiplyExpand(BigNumber x, long y) {
-    BigNumber result = new BigNumber(x.getSize() + 1);
+  public BigNumber multiply (BigNumber y) {
+    BigNumber result = new BigNumber(getSize());
     for (int i = 0; i < result.getSize(); ++i) {
-      result.setLongPart(x.getPartAsLong(i) * y + result.getPartAsLong(i), i);
+      result = result.add(multiply(y.getPartAsLong(i)).leftPartShift(i));
     }
     return result;
   }
 
-  public static BigNumber multiply(BigNumber x, BigNumber y) {
-    BigNumber result = new BigNumber(Math.max(x.getSize(), y.getSize()));
-    for (int i = 0; i < result.getSize(); ++i) {
-      result = result.add(x.multiply(y.getPartAsLong(i)).partShift(i));
-    }
-    return result;
-  }
+  public BigNumber[] divideAndModulo (BigNumber y) {
+    BigNumber quotient = new BigNumber(getSize(), 0);
+    BigNumber remainder = new BigNumber(getSize(), 0);
+    BigNumber divisor = new BigNumber(getSize(), y);
 
-  public static BigNumber[] divideAndModulo (BigNumber x, BigNumber y) {
-    int size = Math.max(x.getSize(), y.getSize());
-    BigNumber quotient = new BigNumber(size, 0);
-    BigNumber remainder = new BigNumber(size, 0);
-    BigNumber divisor = new BigNumber(size, y);
-
-    for (int i = x.getSize() * 32 - 1; i >= 0; --i) {
-      remainder = remainder.bitShift(1);
-      if (x.getBit(i)) {
+    for (int i = getSize() * 32 - 1; i >= 0; --i) {
+      remainder = remainder.leftBitShift(1);
+      if (getBit(i)) {
         remainder.setBit(0);
       }
       if (remainder.moreOrEqualTo(divisor)) {
@@ -201,126 +231,36 @@ public class BigNumber {
     return new BigNumber[]{quotient, remainder};
   }
 
-  public static int compare (BigNumber x, BigNumber y) {
-    for (int i = x.getSize() - 1; i >= 0; --i) {
-      if (x.getPartAsLong(i) < y.getPartAsLong(i)) {
-        return -1;
-      }
-      else if (x.getPartAsLong(i) > y.getPartAsLong(i)) {
-        return 1;
-      }
-    }
-    return 0;
+  public BigNumber divide (BigNumber y) {
+    return divideAndModulo(y)[0];
   }
 
-  public static boolean lessThan (BigNumber x, BigNumber y) {
-    return compare(x, y) < 0;
+  public BigNumber modulo (BigNumber y) {
+    return divideAndModulo(y)[1];
   }
 
-  public static boolean lessOrEqualTo (BigNumber x, BigNumber y) {
-    return compare(x, y) <= 0;
-  }
+  public BigNumber modularExponent (BigNumber modulus, BigNumber exp) {
+    BigNumber ZERO = new BigNumber(getSize(), 0);
+    BigNumber ONE = new BigNumber(getSize(), 1);
 
-  public static boolean equalTo (BigNumber x, BigNumber y) {
-    return compare(x, y) <= 0;
-  }
-
-  public static boolean moreOrEqualTo (BigNumber x, BigNumber y) {
-    return compare(x, y) >= 0;
-  }
-
-  public static boolean moreThan (BigNumber x, BigNumber y) {
-    return compare(x, y) > 0;
-  }
-
-  public BigNumber inverse() {
-    return inverse(this);
-  }
-
-  public BigNumber partShift(int shift) {
-    return partShift(this, shift);
-  }
-
-  public BigNumber bitShift (int shift) {
-    return bitShift(this, shift);
-  }
-
-  public BigNumber add (long other) {
-    return add(this, other);
-  }
-
-  public BigNumber add(BigNumber other) {
-    return add(this, other);
-  }
-
-  public BigNumber negate() {
-    return negate(this);
-  }
-
-  public BigNumber subtract(BigNumber other) {
-    return subtract(this, other);
-  }
-
-  public BigNumber multiply(long other) {
-    return multiply(this, other);
-  }
-
-  public BigNumber multiplyExpand(long other) {
-    return multiplyExpand(this, other);
-  }
-
-  public BigNumber multiply(BigNumber other) {
-    return multiply(this, other);
-  }
-
-  public BigNumber[] divideAndModulo (BigNumber other) {
-    return divideAndModulo(this, other);
-  }
-
-  public BigNumber divide (BigNumber other) {
-    return this.divideAndModulo(other)[0];
-  }
-
-  public BigNumber modulo (BigNumber other) {
-    return this.divideAndModulo(other)[1];
-  }
-
-  public boolean lessThan (BigNumber other) {
-    return lessThan(this, other);
-  }
-
-  public boolean lessOrEqualTo (BigNumber other) {
-    return lessOrEqualTo(this, other);
-  }
-
-  public boolean equalTo (BigNumber other) {
-    return equalTo(this, other);
-  }
-
-  public boolean moreOrEqualTo (BigNumber other) {
-    return moreOrEqualTo(this, other);
-  }
-
-  public boolean moreThan (BigNumber other) {
-    return moreThan(this, other);
-  }
-
-
-  /*public static BigNumber multiply (BigNumber x, BigNumber y) {
-    if (x.getSize() == 1 || y.getSize() == 1) {
-      return new BigNumber(2, x.getPartAsLong(0) * y.getPartAsLong(0));
+    if (modulus.equalTo(ONE)) {
+      return ZERO;
     }
     else {
+      BigNumber result = ONE;
+      BigNumber base = modulo(modulus);
+      BigNumber exponent = new BigNumber(getSize(), exp);
 
-
-      BigNumber z0 = multiply(x0, y0);
-      BigNumber z2 = multiply(x1, y1);
-      BigNumber z1 = multiply(x0.add(x1), y0.add(y1)).subtract(z0.add(z1));
-
-      return z2.partShift(m * 2).add(z1.partShift(m)).add(z0);
+      while (exponent.moreThan(ZERO)) {
+        if (exponent.getPart(0) % 2 == 1) {
+          result = result.multiply(base).modulo(modulus);
+        }
+        exponent = exponent.rightBitShift(1);
+        base = base.multiply(base).modulo(modulus);
+      }
+      return result;
     }
-  }*/
-
+  }
 
   public static BigNumber generateRandom(int size) {
     BigNumber result = new BigNumber(size);
@@ -332,15 +272,8 @@ public class BigNumber {
   }
 
   public static void main(String[] args) {
-    BigNumber n = BigNumber.generateRandom(16);
-    BigNumber d = new BigNumber(16, BigNumber.generateRandom(4));
-    BigNumber[] qd = n.divideAndModulo(d);
-    System.out.println("n = " + n.toHexString());
-    System.out.println("d = " + d.toHexString());
-    System.out.println("q = " + qd[0].toHexString());
-    System.out.println("r = " + qd[1].toHexString());
-    System.out.println("n = d * q + r = " + d.multiply(qd[0]).add(qd[1]).toHexString());
+    BigNumber a = new BigNumber(4, 4).modularExponent(new BigNumber(4, 497), new BigNumber(4, 13));
 
-    System.out.println(new BigNumber(1, 10).modulo(new BigNumber(1, 3)).toHexString());
+    System.out.println(a.toHexString());
   }
 }
