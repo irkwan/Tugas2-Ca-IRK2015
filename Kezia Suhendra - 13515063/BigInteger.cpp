a070603b3da558b9e63aea9d0dd104f1fc27239d
keziasuhendra <keziasuhendra44@gmail.com>
2017-06-28 21:31:25 +0700

    added addition opr

--- Kezia Suhendra - 13515063/BigInteger.cpp
+++ Kezia Suhendra - 13515063/BigInteger.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <iostream>
 #include <algorithm>
+#include <deque>
 #include "BigInteger.h"
 
 using namespace std;
@@ -81,21 +82,102 @@ BigInteger BigInteger::pow(int p) {
   return ex;
 }
 
-BigInteger operator+=(const BigInteger&, const BigInteger&);
-BigInteger operator-=(const BigInteger&, const BigInteger&);
-BigInteger operator*=(const BigInteger&, const BigInteger&);
-BigInteger operator/=(const BigInteger&, const BigInteger&);
-BigInteger operator%=(const BigInteger&, const BigInteger&);
-BigInteger operator+(const BigInteger&, const BigInteger&);
-BigInteger operator++(BigInteger&);
-BigInteger operator++(BigInteger&, int);
-BigInteger operator-(const BigInteger&, const BigInteger&);
-BigInteger operator-(const BigInteger&);
-BigInteger operator--(BigInteger&);
-BigInteger operator--(BigInteger&, int);
-BigInteger operator*(const BigInteger&, const BigInteger&);
-BigInteger operator/(const BigInteger&, const BigInteger&);
-BigInteger operator%(const BigInteger&, const BigInteger&);
+BigInteger operator+=(const BigInteger& num1, const BigInteger& num2) {
+  if (num1.sign == num2.sign) {
+    vector<char>::iterator idx1;
+    vector<char>::const_iterator idx2;
+    idx1 = num1.number.begin();
+    idx2 = num2.number.begin();
+    char ch = 0;
+    while (idx1 != num1.number.end() && idx2 != num2.number.end()) {
+      (*idx1) = (*idx1) + (*idx2) + ch;
+      ch = ((*idx1) > 9);
+      (*idx1) %= 10;
+      ++idx1;
+      ++idx2;
+    }
+    while (idx1 != num1.number.end()) {
+      (*idx1) = (*idx1) + ch;
+       ch = ((*idx1) > 9);
+       (*idx1) %= 10;
+       ++idx1;
+    }
+    while (idx2 != num2.number.end()) {
+      char val = (*idx2) + ch;
+      ch = (val > 9);
+      val %= 10;
+      num1.number.push_back(val);
+      ++idx2;
+    }
+    if (ch != 0) {
+      num1.number.push_back(ch);
+      return num1;
+    }
+  } else {
+    if (num1.sign) {
+      return num1 -= (-num2);
+    } else {
+      return num1 = num2 - (-num1);
+    }
+  }
+}
+
+BigInteger operator-=(const BigInteger&, const BigInteger&) {
+
+}
+
+BigInteger operator*=(const BigInteger&, const BigInteger&) {
+
+}
+
+BigInteger operator/=(const BigInteger&, const BigInteger&) {
+
+}
+
+BigInteger operator%=(const BigInteger&, const BigInteger&) {
+
+}
+
+BigInteger operator+(const BigInteger&, const BigInteger&) {
+
+}
+
+BigInteger operator++(BigInteger&) {
+
+}
+
+BigInteger operator++(BigInteger&, int) {
+
+}
+
+BigInteger operator-(const BigInteger&, const BigInteger&) {
+
+}
+
+BigInteger operator-(const BigInteger&) {
+
+}
+
+BigInteger operator--(BigInteger&) {
+
+}
+
+BigInteger operator--(BigInteger&, int) {
+
+}
+
+BigInteger operator*(const BigInteger&, const BigInteger&) {
+
+}
+
+BigInteger operator/(const BigInteger&, const BigInteger&) {
+
+}
+
+BigInteger operator%(const BigInteger&, const BigInteger&) {
+
+}
+
 bool operator>(const BigInteger&, const BigInteger&);
 bool operator<(const BigInteger&, const BigInteger&);
 bool operator>=(const BigInteger&, const BigInteger&);
