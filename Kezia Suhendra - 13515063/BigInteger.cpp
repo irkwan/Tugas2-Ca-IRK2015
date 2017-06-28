0ddda4d90b1024948181946c87024909847bed5a
keziasuhendra <keziasuhendra44@gmail.com>
2017-06-28 19:10:32 +0700

    added constructor, opr=, etc.

--- Kezia Suhendra - 13515063/BigInteger.cpp
+++ Kezia Suhendra - 13515063/BigInteger.cpp
@@ -6,14 +6,81 @@
 
 using namespace std;
 
-BigInteger::BigInteger();
-BigInteger::BigInteger(int);
-BigInteger::BigInteger(string&);
-BigInteger::BigInteger(const BigInteger&);
-BigInteger BigInteger::operator=(const BigInteger& opr);
-void BigInteger::trimZero();
-BigInteger BigInteger::absolute() const;
-BigInteger BigInteger::pow(int p);
+BigInteger::BigInteger() {
+  sign = true;
+}
+
+BigInteger::BigInteger(int num) {
+  if (num >= 0) {
+    sign = true;
+  } else {
+    sign = false;
+    num *= (-1);
+  }
+  do {
+    number.push_back((char)(num % 10));
+    num /= 10;
+  } while (num != 0)
+}
+
+BigInteger::BigInteger(string& num) {
+  sign = true;
+  for (string::reverse_iterator idx = num.rbegin(); idx < num.rend(); ++idx) {
+    char c = (*idx);
+    if (idx == num.rend()-1) {
+      if (c == '+') {
+        break;
+      }
+      if (c == '-') {
+        sign = false;
+        break;
+      }
+    }
+  }
+  trimZero();
+}
+
+BigInteger::BigInteger(const BigInteger& big) {
+  number = big.number;
+  sign = big.sign;
+}
+
+BigInteger BigInteger::operator=(const BigInteger& big) {
+  if (this != &big) {
+    number = big.number;
+    sign = big.sign;
+  }
+  return * this;
+}
+
+void BigInteger::trimZero() {
+  vector<char>::reverse_iterator idx = number.rbegin();
+  while (!number.empty() && (*idx) == 0) {
+    number.pop_back();
+    idx = number.rbegin();
+  }
+  if (number.size() == 0) {
+    sign = true;
+    number.push_back(0);
+  }
+}
+
+BigInteger BigInteger::absolute() const {
+  if (sign) {
+    return * this;
+  } else {
+    return -(*this);
+  }
+}
+
+BigInteger BigInteger::pow(int p) {
+  BigInteger ex(1);
+  for (int i = 0; i < p; ++i) {
+    ex *= (*this);
+  }
+  return ex;
+}
+
 BigInteger operator+(const BigInteger&, const BigInteger&);
 BigInteger operator++(BigInteger&);
 BigInteger operator++(BigInteger&, int);
