b3c3c30553b441a10378f785fb7ef5cae5fd407b
Dery Rahman A <dery.ra@gmail.com>
2017-07-11 13:29:15 +0700

    Remove Add and Minus, add divmod and power base10, remove divmod

--- Dery - 13515097/BigIntMultiplication.py
+++ Dery - 13515097/BigIntMultiplication.py
@@ -1,85 +1,34 @@
-"""
-	Addition Operator
-"""
-def add(x,y):
+def divmod10(x,y):
 	x = str(x)
 	y = str(y)
+	return int(x[:len(x)-len(y)+1]),int(x[-len(y)+1:])
 
-	if len(x)<len(y):
-		temp = x
-		x = y
-		y = temp
-	min = len(y)
-	reminder = 0
-	result = " "
-	for i in range(min):
-		curr = int(x[len(x)-1-i])+int(y[len(y)-1-i])+reminder
-		reminder = curr/10
-		curr = curr%10
-		result = str(curr)+result
-
-	x = list(x)
-	x[len(x)-len(y)-1] = str(int(x[len(x)-len(y)-1])+reminder)
-	x="".join(x)
-	result = x[:len(x)-len(y)] + result
-	return result
+# global ten_pow
+ten_pow = {}
+for i in range(1000):
+	if i==0:
+		ten_pow[i]=1
+	else:
+		ten_pow[i]=ten_pow[i-1]*10
 
 """
-	Minus Operator
+	Binary Expo
 """
-def min(x,y):
-	x = str(x)
-	y = str(y)
-	if len(x)<len(y):
-		temp = x
-		x = y
-		y = temp
-	min = len(y)
-	reminder = 0
-	result = " "
-	for i in range(min):
-		curr = int(x[len(x)-1-i])+int(y[len(y)-1-i])-reminder
-		if curr<0:
-			reminder = 1
-			curr = 10+curr
-		else:
-			reminder = 0
-		result = str(curr)+result
-
-	x = list(x)
-	x[len(x)-len(y)-1] = str(int(x[len(x)-len(y)-1])-reminder)
-	x="".join(x)
-	result = x[:len(x)-len(y)] + result
-	return result
-
-def divmod(x,y):
-	q=0
-	while x>=y:
-		q+=1
-		x-=y
-	return q,x
-
-"""
-	Binary Exponential
-"""
-MOD = 1000000007
 def pow(x,y):
-	result=1
-	while y>0:
-		# If power is odd
-		if y%2==1:
-			dummy,result=divmod((mul(result,x)),MOD)
-			# Divide the power by 2
-			y,dummy = divmod(y,2)
-			# Multiply base to itself
-			dummy,x = divmod((mul(x,x)),MOD)
-	return result
+	result=1;
+	while (y!=0):
+		if (y&1==1):
+			result*=x
+		y>>=1
+		x*=x
+	# print result
+	return result;
 
 """
 	Karatsuba Multiplication
 """
 def mul(x,y):
-	if (len(str(x))<19 or len(str(y))<19): # if length of number below length of 2^64-1 bit, then multiply like a normal
+	if (len(str(x))==1 or len(str(y))==1):
 		return x*y
 	else:
 		n = max(len(str(x)),len(str(y)))
@@ -91,19 +40,25 @@ def mul(x,y):
 				x = x1.B^m + x0
 				y = y1.B^m + y0
 		"""
-		x1,x0 = divmod(x,pow(B,m))
-		y1,y0 = divmod(y,pow(B,m))
+		x1,x0 = divmod10(x,ten_pow[m]) # x/B**m,x%B**m
+		y1,y0 = divmod10(y,ten_pow[m]) # y/B**m,y%B**m
 
-		# Compute three multiplications, with recursive
+		"""
+			Compute three multiplications, with recursive
+		"""
 		z0 = mul(x0,y0)
-		z1 = mul(add(x1,x0),add(y1,y0))
+		z1 = mul(x1+x0,y1+y0)
 		z2 = mul(x1,y1)
 
-		# Result
-		result = add(mul(z2,pow(B,mul(2,m))),(add(mul((min(z1,min(z2,z0))),pow(B,m)),z0)))
+		"""
+			Result
+		"""
+		# print z2, z1, z0
+		result = z2 * ten_pow[2*m] + ((z1-z2-z0) * ten_pow[m]) + z0
 		return result
 
-x = 102131243242432
-y = 1243231234432432
+x = 102131243242432123627183298732895738459465
+y = 124323123443243238764283289832740832702837
 print mul(x,y)
-# print divmod(14,3)
+# print pow(3,200)
+# print divmod10(144,100)
