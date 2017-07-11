d840fcf1a8198868431eb793e78aa68f4fde7e26
Dery Rahman A <dery.ra@gmail.com>
2017-07-11 10:31:54 +0700

    Add divmod(not optimal) and pow(Binary Exponential) function

--- Dery - 13515097/BigIntMultiplication.py
+++ Dery - 13515097/BigIntMultiplication.py
@@ -1,12 +1,35 @@
+def divmod(x,y):
+	q=0
+	while x>=y:
+		q+=1
+		x-=y
+	return q,x
+
+"""
+	Binary Exponential
+"""
+MOD = 1000000007
+def pow(x,y):
+	result=1
+	while y>0:
+		# If power is odd
+		if y%2==1:
+			result=(mul(result,x))%MOD
+			# Divide the power by 2
+			y = y/2
+			# Multiply base to itself
+			x = (mul(x,x))%MOD
+	return result
+
 """
 	Karatsuba Multiplication
 """
 def mul(x,y):
-	if (len(str(x))==1 or len(str(y))==1):
+	if (len(str(x))<19 or len(str(y))<19): # if length of number below length of 2^64-1 bit, then multiply like a normal
 		return x*y
 	else:
 		n = max(len(str(x)),len(str(y)))
-		m = n / 2
+		m = n/2
 		B = 10
 
 		"""
@@ -14,24 +37,19 @@ def mul(x,y):
 				x = x1.B^m + x0
 				y = y1.B^m + y0
 		"""
-		x1 = x / B**(m)
-		x0 = x % B**(m)
-		y1 = y / B**(m)
-		y0 = y % B**(m)
+		x1,x0 = divmod(x,pow(B,m))
+		y1,y0 = divmod(y,pow(B,m))
 
-		"""
-			Compute three multiplications, with recursive
-		"""
+		# Compute three multiplications, with recursive
 		z0 = mul(x0,y0)
 		z1 = mul(x1+x0,y1+y0)
 		z2 = mul(x1,y1)
 
-		"""
-			Result
-		"""
-		result = z2 * B**(2*m) + ((z1-z2-z0) * B**m) + z0
+		# Result
+		result = mul(z2,pow(B,mul(2,m))) + mul((z1-z2-z0),pow(B,m)) + z0
 		return result
 
-x = 1324354647489202004938573894030298473858930827492
-y = 7384723943283474329472364938761038746104361086412634
+x = 102131243242432
+y = 1243231234432432
 print mul(x,y)
+# print divmod(14,3)
