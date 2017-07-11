8aad3ba9724461576500896bbdcc4fc30d6bf714
Dery Rahman A <dery.ra@gmail.com>
2017-07-11 16:21:09 +0700

    Optimize Addition, Substraction, Division, Modulus, and Multiplication

--- Dery - 13515097/BigIntMultiplication.py
+++ Dery - 13515097/BigIntMultiplication.py
@@ -1,8 +1,96 @@
+"""
+	Comparation
+	equal 0
+	greater than 1
+	less than 2
+"""
+def cmp(x,y):
+	x = str(x)
+	y = str(y)
+
+	if(len(x)>len(y)):
+		return 1
+	if(len(x)<len(y)):
+		return 2
+
+	for i in range(len(x)):
+		if(x[i]>y[i]):
+			return 1
+		if(x[i]<y[i]):
+			return 2
+	return 0
+
+"""
+	Addition Operator
+"""
+def add(x,y):
+	x = str(x)
+	y = str(y)
+
+	if len(x)<len(y):
+		temp = x
+		x = y
+		y = temp
+	max = len(x)
+	for i in range(max-len(y)):
+		y = "0"+y
+	reminder = 0
+	result = " "
+	for i in range(max):
+		curr = int(x[len(x)-1-i])+int(y[len(y)-1-i])+reminder
+		reminder = curr/10
+		curr = curr%10
+		result = str(curr)+result
+	result = str(reminder) + result
+	return int(result)
+
+"""
+	Substraction Operator
+	x always >= y
+"""
+def sub(x,y):
+	# if(cmp(x,y)==2): # less than
+	# 	temp = x
+	# 	x = y
+	# 	y = temp
+	# print x, y
+	x = str(x)
+	y = str(y)
+	max = len(x)
+	for i in range(max-len(y)):
+		y = "0"+y
+	reminder = 0
+	result = " "
+	for i in range(max):
+		# print x[len(x)-1-i], y[len(x)-1-i]
+		curr = int(x[len(x)-1-i])-int(y[len(y)-1-i])-reminder
+		if curr<0:
+			reminder = 1
+			curr = 10+curr
+		else:
+			reminder = 0
+		result = str(curr)+result
+
+	# x = list(x)
+	# x[len(x)-len(y)-1] = str(int(x[len(x)-len(y)-1])-reminder)
+	# x="".join(x)
+	# result = x[:len(x)-len(y)] + result
+	return int(result)
+
+
+"""
+	Division and Modulo with base10
+"""
 def divmod10(x,y):
 	x = str(x)
 	y = str(y)
+	if(len(y)>len(x)):
+		return 0,int(x)
 	return int(x[:len(x)-len(y)+1]),int(x[-len(y)+1:])
 
+"""
+	Power with base10
+"""
 # global ten_pow
 ten_pow = {}
 for i in range(1000):
@@ -12,20 +100,17 @@ for i in range(1000):
 		ten_pow[i]=ten_pow[i-1]*10
 
 """
-	Binary Expo
+	Multiplication with base10
+	x = Number
+	y = Number with base10 ex. 10000
 """
-def pow(x,y):
-	result=1;
-	while (y!=0):
-		if (y&1==1):
-			result*=x
-		y>>=1
-		x*=x
-	# print result
-	return result;
+def mul10(x,y):
+	x = str(x)
+	x += str(y)[1:]
+	return int(x)
 
 """
-	Karatsuba Multiplication
+	Karatsuba Multiplication with base10
 """
 def mul(x,y):
 	if (len(str(x))==1 or len(str(y))==1):
@@ -34,7 +119,6 @@ def mul(x,y):
 		n = max(len(str(x)),len(str(y)))
 		m = n/2
 		B = 10
-
 		"""
 			Split number into form
 				x = x1.B^m + x0
@@ -47,18 +131,15 @@ def mul(x,y):
 			Compute three multiplications, with recursive
 		"""
 		z0 = mul(x0,y0)
-		z1 = mul(x1+x0,y1+y0)
+		z1 = mul((x1+x0),(y1+y0))
 		z2 = mul(x1,y1)
 
 		"""
 			Result
 		"""
-		# print z2, z1, z0
-		result = z2 * ten_pow[2*m] + ((z1-z2-z0) * ten_pow[m]) + z0
+		result = add(mul10(z2,ten_pow[2*m]),add(mul10((sub(z1,add(z2,z0))),ten_pow[m]),z0)) # z2*(B**(2*m))+(z1-z2-z0)*(B**m)+z0
 		return result
 
-x = 102131243242432123627183298732895738459465
-y = 124323123443243238764283289832740832702837
+x = 102131243242432123627183298732895738436453563465346534634653456764567456747654354353453345635634654634653465454354559465
+y = 124323123443243238764283289832740834653465436534653456346346327028345345334534545346534563463564576456745673465654453537
 print mul(x,y)
-# print pow(3,200)
-# print divmod10(144,100)
