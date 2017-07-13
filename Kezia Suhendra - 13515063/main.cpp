1042c49995990f7fd709b29a27ea2deb4ab339e5
keziasuhendra <keziasuhendra44@gmail.com>
2017-07-13 20:55:45 +0700

    almost final

--- Kezia Suhendra - 13515063/main.cpp
+++ Kezia Suhendra - 13515063/main.cpp
@@ -5,53 +5,53 @@
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
+#include <fstream>
 #include "BigInteger.h"
 #include "StringTrans.h"
 #include "RSA.h"
 #include "OAEP.h"
+#include <ctime>
 
 using namespace std;
 
 int main(int argc, char **argv) {
-  if (argc != 2) {
-    printf("How to execute: ./<exec> <either 512 / 768 / 1024 / 2048>");
+  if (argc != 3) {
+    printf("How to execute: ./<exec> <either 512 / 768 / 1024 / 2048> <file name>");
     exit(EXIT_FAILURE);
   }
   int keyLength;
-  if (string(argv[1]) != "fast") {
-    keyLength = atoi(argv[1]);
-    if (keyLength < 256) {
-      printf("Input the number either 512 or 768 or 1024 or 2048 after ./<exec>\n");
-      exit(EXIT_FAILURE);
-    }
-    switch(keyLength) {
-      case 512:
-      case 768:
-      case 1024:
-      case 2048:
-        break;
-      default:
-        printf("Input the number either 512 or 768 or 1024 or 2048 after ./<exec>\n");
-        exit(EXIT_FAILURE);
-    }
-  } else if (string(argv[1]) == "fast") {
-    keyLength = -1;
-  } else {
-    fprintf(stderr, "Invalid Argument %s\n", argv[1]);
+  keyLength = atoi(argv[1]);
+  if (keyLength < 256) {
+    printf("Input the number either 512 or 768 or 1024 or 2048 after ./<exec>\n");
     exit(EXIT_FAILURE);
   }
-  cout << "\nInput your message here:\n";
-  string msg;
-  getline(cin, msg);
-  RSA *rsa;
-  if(keyLength != -1) {
-    rsa = new RSA(keyLength);
-  } else {
-    BigInt strN("838BB8FFAFF51B40E52DF0DB1F0C3466EE14C46A37C973BBD1718A15023F55EA683D0A6B93F3DD0155E9658B6A413D6715E7B8C1E85BC4167CB9914F5E2137C9", 16);
-    BigInt strE("ACC86137DDC424D1C24331E10BF9239A49001F34B16BE4B80E39F8D00CD6FA99", 16);
-    BigInt strD("39E75F3757C1A40A87493AA67EDB38B7BD31954FF184BD24698A66309CED1B7A0C423D0799D42B96815D7A8009EC5F4CB66628E9657004995FDFB5865B10C3F9", 16);
-    rsa = new RSA(strN, strE, strD);
+  switch(keyLength) {
+    case 512:
+    case 768:
+    case 1024:
+    case 2048:
+      break;
+    default:
+      printf("Input the number either 512 or 768 or 1024 or 2048 after ./<exec>\n");
+      exit(EXIT_FAILURE);
   }
+  /*cout << "\nInput your message here:\n";
+  string msg;
+  getline(cin, msg);*/
+  ifstream input(argv[2]);
+  string msg;
+  char c;
+  while (!input.eof()) {
+    c = input.get();
+    if (c != '\0') {
+      msg += c;
+    }
+  }
+  input.close();
+  msg.pop_back();
+  RSA *rsa;
+  auto begin = chrono::high_resolution_clock::now();
+  rsa = new RSA(keyLength);
   BigInt N, E;
   rsa->getPublicKey(N, E);
   StringTrans st(msg, N.GetBitLength() - 17);
@@ -59,17 +59,19 @@ int main(int argc, char **argv) {
   cout << st.toString() << endl;
   OAEP oaep(16, N.GetBitLength() - 17);
   oaep.encode(st);
-  cout << "\nEncoded Message:\n";
-  cout << st.toHexString() << endl;
+  //cout << "\nEncoded Message:\n";
+  //cout << st.toHexString() << endl;
   RSA::encrypt(st, N, E);
   cout << "\nEncrypted Message:\n";
   cout << st.toHexString() << endl;
+  auto end = chrono::high_resolution_clock::now();
   rsa->decrypt(st);
   cout << "\nDecryped Message:\n";
   cout << st.toHexString() << endl;
   oaep.decode(st);
   cout << "\nDecoded Message:\n";
   cout << st.toString() << endl << endl;
+  cout << "Time elapsed: " << chrono::duration_cast<chrono::milliseconds>(end-begin).count() << " ms" << endl << endl;
 
   return 0;
 }
